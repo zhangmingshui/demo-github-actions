ff8c87b6677d239e69eeed8fb1ba41863230b110
steve chong <dr.steve.chong@gmail.com>
2022-08-28 10:01:03 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("HELLO");
+        }
+    }
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("BYE!!!!");
+    }
+
+}
